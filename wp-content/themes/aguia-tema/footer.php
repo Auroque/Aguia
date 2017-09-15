2a35cf6346741ba5ea67ec90bb5361cf76e75c8d
Luiz Augusto Silva Veloso <luizaugusto_sv@hotmail.com>
2017-09-15 20:43:16 -0300

    footer pronto

--- wp-content/themes/aguia-tema/footer.php
+++ wp-content/themes/aguia-tema/footer.php
@@ -11,6 +11,11 @@
 					<?php if ( dynamic_sidebar('rodape_widgets-3') ) : else : endif; ?>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-md-12 cop">
+					<p>Águia Sul - Goiás | Desenvolvido por Luiz Augusto</p>
+				</div>
+			</div>
 		</div>
 	</div>
 	<script src="<?php bloginfo('template_directory'); ?>/js/jquery-3.2.1.min.js"></script>
