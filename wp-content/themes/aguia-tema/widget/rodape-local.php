05fd67df22abdbe4e85df50870f1699687d50b34
Luiz Augusto Silva Veloso <luizaugusto_sv@hotmail.com>
2017-09-16 13:46:37 -0300

    Página Produto Pronto. (Post category)

--- wp-content/themes/aguia-tema/widget/rodape-local.php
+++ wp-content/themes/aguia-tema/widget/rodape-local.php
@@ -6,6 +6,15 @@
 function widgets_novos_widgets_init() {
 
 	register_sidebar( array(
+		'name' => 'Categorias',
+		'id' => 'widgets-categoria',
+		'before_widget' => '<div>',
+		'after_widget' => '</div>',
+		'before_title' => '<h2>',
+		'after_title' => '</h2>',
+	) );
+
+	register_sidebar( array(
 		'name' => 'Rodape',
 		'id' => 'rodape_widgets',
 		'before_widget' => '<div>',
